24391c07bbd66d51cd12a01f5609bba6148246d4
Deopster <andrey11551611@gmail.com>
2021-09-12 20:51:03 +0300

    Revert "Delited all files"
    This reverts commit 6034b8f463ae39169b389b85ce941c02402b6784.

---
+++ CalcMat.php
@@ -0,0 +1,17 @@
+<?php
+$link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
+mysqli_query($link, "SET NAMES 'utf8'");
+?>
+<table> <tr > <th>№</th><th width="100%">Ф.И.О. </th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th> <th data-tooltip=«дата»>🗓</th><th data-tooltip=«дата»>🗓</th>
+<?php $query = "SELECT * FROM calcmat WHERE id > 0";
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row); $result = ''; foreach ($data as $elem) { 
+$result .= '<tr>'; 
+$result .= '<td id="trig">' . $elem['id'] . '</td>';
+$result .= '<td id="trig">' . $elem['name'] . '</td>';
+for ($x=1; $x<37; $x++){
+ $result .= '<td id="calcmat">' . $elem[$x.'A'] . '</td>'; 
+ }
+} 
+echo $result; ?>
+</table>
