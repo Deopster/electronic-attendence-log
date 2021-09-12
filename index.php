f5d7b45e2e04169cf5cef2a9063b52a56e50c900
Deopster <andrey11551611@gmail.com>
2021-09-12 16:24:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,157 @@
 ﻿<?php
-
-session_start();
+require_once("connect.php");
+session_start()
 ?>
+<!DOCTYPE html>
+<head>
+<html lang="en" class="no-js">
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>IKBO-12-19</title>
+	<link rel="stylesheet" type="text/css" href="css/dopblock.css" />
+	<link rel="stylesheet" type="text/css" href="css/mainblock.css" />
+    <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+	<link rel="stylesheet" type="text/css" href="css/tables.css" />
+	<link rel="stylesheet" type="text/css" href="css/reg.css" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="js/modernizr.custom.js"></script>
+	<!--костыль-->
+	<script src="js/kost.js"></script>
+	<!--каледарная подсказка-->
+	<script src="js/showme.js"></script>
+	<!--онлайн редакция  -->
+	<script src="js/editthis.js"></script>
+	</head>
+
+<body >
+	<div id="st-container" class="st-container">
+		<!--ВЫдвижная панель-->
+		<nav class="st-menu st-effect-11" id="menu-11">
+			<a href="./index.html"><img src="img/fin.png"  width="280" alt="Logo"></a>
+			<!--кнопка выдвижной панели-->
+			<div id="st-trigger-effects" class="news">
+				<button data-effect="st-effect-11">
+				</button>
+			</div>
+			<div id="st-menu" class="news1">
+				<button st-menu="close">
+				</button>
+			</div>
+			
+
+			<ul id "menu">
+				<li><a href="#AOSMD"onClick="change(1)">Архитектура оперционных систем мобильных устройств</a></li>
+				<li><a href="#Matan"onClick="change(2)">Математический анализ</a></li>
+				<li><a href="#Konf"onClick="change(3)">Конфигурационное управление</a></li>
+				<li><a href="#CalcMat"onClick="change(4)">Вычислительная математика</a></li>
+				<li><a href="#filosof"onClick="change(5)">Философия</a></li>
+				<li><a href="#ACM&S"onClick="change(6)">Архитектура вычислительных машин и систем</a></li>
+				<li><a href="#SIAOD"onClick="change(7)">Структуры и алгоритмы обработки данных</a></li>
+				<li><a href="#Eng"onClick="change(8)">Иностранный язык</a></li>
+				<li><a href="#Java"onClick="change(9)">Программирование на яп Java</a></li>
+			</ul>
+			<div class="">
+			</div>
+		</nav>
+		
+		<div class="st-pusher">
+			<div class="st-content">
+				<!--эффект форточки-->
+				<div class="st-content-inner">
+					<!--тута основная часть-->
+					    <!---вызов сиаод-->
+						<div class="table-block1"id="AOSMD" >
+						<div class="intable-block"align="center"><h3 >Архитектура операционных систем мобильных устройств</h3></div>
+						<?php
+						require_once("AOSMD.php");					
+						?>
+						</div>
+				
+						<!---вызов матана-->
+						<div class="table-block"id="Matan" >
+						<div class="intable-block"align="center"><h3 >Математический анализ</h3></div>
+						<?php
+						require_once("Matan.php");
+						?>
+						</div>
+						<!---вызов конфигурационки-->
+						<div class="table-block"id="Konf">
+						<div class="intable-block"align="center"><h3 >Конфигурационное управление</h3></div>
+					    <?php
+						require_once("Konf.php");
+						?>
+						</div>
+						<!---вызов выч матана-->
+						<div class="table-block"id="CalcMat">
+						<div class="intable-block"align="center"><h3 >Вычислительная математика</h3></div>
+						<?php
+						require_once("CalcMat.php");
+						?>
+						</div>
+						 <!---вызов философия-->
+						<div class="table-block"id="filosof">
+						<div class="intable-block"align="center"><h3 >Философия</h3></div>
+						<?php
+						require_once("filosof.php");
+						?>
+						</div>
+					     <!---вызов Архитектуры вычислительных машин и систем-->
+						<div class="table-block"id="ACM&S">
+						<div class="intable-block"align="center"><h3 >Архитектура вычислительных машин и систем</h3></div>
+						<?php
+						require_once("ACM&S.php");
+						?>
+						</div>
+						 <!---вызов СИАОД-->
+						<div class="table-block"id="SIAOD">
+						<div class="intable-block"align="center"><h3 >Структуры и алгоритмы обработки данных</h3></div>
+						<?php
+						require_once("SIAOD.php");
+						?>
+						</div>
+						 <!---вызов Инглиша-->
+						<div class="table-block"id="Eng">
+						<div class="intable-block"align="center"><h3 >Иностранный язык</h3></div>
+						<?php
+						require_once("eng.php");
+						?>
+						</div>
+						 <!---вызов Java-->
+						<div class="table-block"id="Java">
+						<div class="intable-block"align="center"><h3 >Программирование на яп Java</h3></div>
+						<?php
+						require_once("Java.php");
+						?>
+						</div>
+					     <!--доп блок-->
+					    <div class="dop-block">
+						<div class="reg-block">
+						<div class="inreg-block"align="center"><h3 >Авторизация</h3></div>
+						
+						<!--пошла жара-->
+						<?php
+						require_once("reg.php");
+						?>	
+						<!--финишная прямая-->
+						</div>
+						<div class="about-block"> <h2>Сайт создан Ефимовым Андреем в качестве курсовой работы.<h2>
+						<div class="inblock"align="center"><h3 >©2020</h3></div>
+						</div>
+						</div>
+						  <!--доп блок конец-->
+						<button data-effect="st-effect-11">
+						</button>
+						
+				</div>
+			</div>
+		</div>
+	<script src="js/classie.js"></script>
+	<script src="js/sidebarEffects.js"></script>
+
+	<!--библиотеки-->
+
+</body>
+</html>
